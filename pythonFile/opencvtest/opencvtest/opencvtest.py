f0a011f730ae8c73896419687f820bf4a1fc4f6f
김태오 <38588097+Sunday5214@users.noreply.github.com>
2019-07-17 10:49:42 +0900

    updateppt

--- pythonFile/opencvtest/opencvtest/opencvtest.py
+++ pythonFile/opencvtest/opencvtest/opencvtest.py
@@ -6,35 +6,26 @@ import threading
 import time
 import pymysql
 import datetime
-##웹캠에서 영상을 읽어온다
-#cap = cv2.VideoCapture(0)
-#cap.set(3, 640) #WIDTH
-#cap.set(4, 480) #HEIGHT
+#웹캠에서 영상을 읽어온다
+cap = cv2.VideoCapture(0)
+cap.set(3, 640) #WIDTH
+cap.set(4, 480) #HEIGHT
 
-##얼굴 인식 캐스케이드 파일 읽는다
-#face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#얼굴 인식 캐스케이드 파일 읽는다
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-#while(True):
-#    # frame 별로 capture 한다
-#    ret, frame = cap.read()
+while(True):
+    # frame 별로 capture 한다
+    ret, frame = cap.read()
 
-#    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-#    #인식된 얼굴 갯수를 출력
-#    print(len(faces))
+    #인식된 얼굴 갯수를 출력
+    print(len(faces))
 
-#    # 인식된 얼굴에 사각형을 출력한다
-#    for (x,y,w,h) in faces:
-#         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-
-#    #화면에 출력한다
-#    cv2.imshow('frame',frame)
-#    if cv2.waitKey(1) & 0xFF == ord('q'):
-#        break
-
-#cap.release()
-#cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 sensor = Adafruit_DHT.DHT11
